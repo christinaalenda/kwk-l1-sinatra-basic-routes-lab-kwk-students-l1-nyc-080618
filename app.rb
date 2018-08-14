5542ab77eeb68ce11105263053c6953fb48deae2
christinaalenda <clenda041@gmail.com>
2018-08-14 19:00:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "My name is Christina!"
+    "My name is Christina"
   end
   get '/hometown' do
-    "My hometown is Paramus!"
+    "My hometown is Paramus"
   end
   get '/favorite-song' do
-    "My favorite song is Hello!"
+    "My favorite song is Hello"
   end
   
 end
